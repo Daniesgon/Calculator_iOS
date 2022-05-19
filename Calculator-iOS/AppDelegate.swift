4d176003a315941481cd0ca2805652f1ab0bb705
D.Escandell <DESCANDELL90@GMAIL.COM>
2022-05-19 19:28:09 +0200

    create project

---
+++ Calculator-iOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Calculator-iOS
+//
+//  Created by Daniel Escandell González on 19/5/22.
+//  Copyright © 2022 Daniel Escandell González. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+

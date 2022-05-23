dce06650716c2826bd439211ae83ce328cf13d54
D.Escandell <DESCANDELL90@GMAIL.COM>
2022-05-23 22:03:36 +0200

    define main

---
+++ Calculator-iOS/Controller/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Calculator-iOS
+//
+//  Created by Daniel Escandell González on 23/5/22.
+//  Copyright © 2022 Daniel Escandell González. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Initialization
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
